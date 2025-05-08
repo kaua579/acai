482243fbdc0ce9412a25e5eac28b53549abaa8e9
caroline2025 <carolzinha8641@gmail.com>
2025-05-08 13:57:54 -0300

    salvar usuario
    fiz um teste no salvar usuario

---
+++ codigo/tests/teste_salvarUsuario.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "../conexao.php";
+    require_once "../funcoes.php";
+
+    $nome = "Fulano";
+    $email = "sla2@gmail.com";
+    $senha = "123";
+    $tipo = "Cliente"
+
+    salvarUsuario($conexao, $nome, $email, $senha , $Tipo);
+?>
