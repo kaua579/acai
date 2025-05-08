26e22723f43e8614293da5db51a5464a4e1fcead
laravtlp <laralopespugas7@gmail.com>
2025-05-08 14:30:33 -0300

    editar cliente
    fiz o editar cliente

--- codigo/conexao.php
+++ codigo/conexao.php
@@ -2,6 +2,6 @@
 $servidor = 'db';
 $usuario = 'root';
 $password = '123';
-$banco = 'banco_acai';
+$banco = 'acai';
 
 $conexao = mysqli_connect($servidor, $usuario, $password, $banco);
--- codigo/funcoes.php
+++ codigo/funcoes.php
@@ -49,7 +49,16 @@ function listarClientes($conexao,) {
     return $lista_clientes;
 }
 
-function editarCliente($conexao, $cpf, $endereco, $idcliente) {}
+function editarCliente($conexao, $cpf, $endereco, $idcliente) {
+    $sql = "UPDATE tb_cliente SET cpf=?, endereco=? WHERE idcliente=?";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'sssi', $cpf, $endereco, $idcliente);
+    $funcionou = mysqli_stmt_execute($comando);
+
+    mysqli_stmt_close($comando);
+    return $funcionou;    
+}
 
 function deletarCliente($conexao, $idcliente){}
 
