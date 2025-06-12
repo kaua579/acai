92bb0a04f3fa44a2b7cac3d2a6d82c2d9b3234da
caroline2025 <carolzinha8641@gmail.com>
2025-06-12 13:54:29 -0300

    mds
    kauã chato

--- codigo/tests/teste_salvarUsuario.php
+++ codigo/tests/teste_salvarUsuario.php
@@ -2,9 +2,9 @@
     require_once "../conexao.php";
     require_once "../funcoes.php";
 
-    $nome = "Fulano";
-    $email = "fulano@gmail.com";
-    $senha = "9999";
+    $nome = "lara";
+    $email = "lara@gmail";
+    $senha = "321";
     $tipo = "C";
 
     salvarUsuario($conexao, $nome, $email, $senha , $tipo);
