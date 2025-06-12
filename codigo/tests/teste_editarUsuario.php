18aca6a793106e43b9fa32f4b1d03fa6e6bf1a89
caroline2025 <carolzinha8641@gmail.com>
2025-06-12 14:22:07 -0300

    o teste listar usuario
    ele deu certo

--- codigo/tests/teste_editarUsuario.php
+++ codigo/tests/teste_editarUsuario.php
@@ -2,13 +2,13 @@
     require_once "../funcoes.php";
     require_once "../conexao.php";
     
-    $nome = 
-    $email = 
-    $senha =
-    $tipo = 
+    $nome = "lara" ;
+    $email = "lara@gmail";
+    $senha = "321";
+    $tipo = "e";
 
         
-editarUsuario($conexao, $nome, $email , $senha , $tipo, );
+    editarUsuario($conexao, $nome, $email , $senha , $tipo , $id);
     
 
 
