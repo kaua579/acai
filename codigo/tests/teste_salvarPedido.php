54a03c547afdab2674fd4e00e2067a2439baba5e
laravtlp <laralopespugas7@gmail.com>
2025-06-12 13:50:31 -0300

    testes
    pedido

---
+++ codigo/tests/teste_salvarPedido.php
@@ -0,0 +1,16 @@
+<?php
+    require_once "../conexao.php";
+    require_once "../funcoes.php";
+
+    
+    $observacao = 
+    $horario_inicio =
+    $horario_final =
+    $status =
+    $valor_p =
+    $taxa_entraga =
+    $data =
+    $nota_atedente =
+
+    salvarPedido($conexao, $observacao, $horario_inicio, $horario_final, $status, $valor_p, $taxa_entraga, $data, $nota_atedente);
+?>
