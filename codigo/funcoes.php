47d6f3ea7617ab9a3de55029bdc47d8d52818d04
kaua579 <kauahenriquemoraissantos@gmail.com>
2025-06-16 13:53:48 -0300

    salvar pedido
    salvar pedido

--- codigo/funcoes.php
+++ codigo/funcoes.php
@@ -228,12 +228,12 @@ function pesquisarProdutoId($conexao, $idproduto)
 ///////////////////////////////////////////////////////////////////////////////////
 
 // pedido
-function salvarPedido($conexao, $observacao, $horario_inicio, $horario_final, $status, $valor_p, $taxa_entraga, $data, $nota_atedente)
+function salvarPedido($conexao, $observacao, $horario_inicio, $horario_final, $status, $valor_p, $taxa_entrega, $data, $nota_atendente, $tb_endereco_idendereco, $tb_atendente_idatendente, $tb_cliente_idcliente) // feito
 {
-    $sql = "INSERT INTO tb_produto (observacao, horario_inicio, horario_final, status, valor_p, taxa_entraga, data, nota_atedente) VALUES (?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO tb_pedido (observacao, horario_inicio, horario_final, status, valor_p, taxa_entrega, data, nota_atendente, tb_endereco_idendereco, tb_atendente_idatendente, tb_cliente_idcliente) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $comando = mysqli_prepare($conexao, $sql);
 
-    mysqli_stmt_bind_param($comando, 'sssssss', $observacao, $horario_inicio, $horario_final, $status, $valor_p, $taxa_entraga, $data, $nota_atedente);
+    mysqli_stmt_bind_param($comando, 'ssssdisdiii', $observacao, $horario_inicio, $horario_final, $status, $valor_p, $taxa_entrega, $data, $nota_atendente, $tb_endereco_idendereco, $tb_atendente_idatendente, $tb_cliente_idcliente);
 
     mysqli_stmt_execute($comando);
 
@@ -241,7 +241,7 @@ function salvarPedido($conexao, $observacao, $horario_inicio, $horario_final, $s
     
 }
 
-function listarPedido($conexao,)
+function listarPedido($conexao,) // feito
 {
     $sql = "SELECT * FROM tb_pedido";
     $comando = mysqli_prepare($conexao, $sql);
--- codigo/tests/teste_salvarPedido.php
+++ codigo/tests/teste_salvarPedido.php
@@ -8,9 +8,12 @@
     $horario_final = "17:00";
     $status = "pronto para entrega";
     $valor_p = "20,00";
-    $taxa_entraga = "3,00";
+    $taxa_entrega = "3,00";
     $data = "2023-03-17";
-    $nota_atedente = "4/5";
+    $nota_atendente = "4/5";
+    $tb_endereco_idendereco = 1;
+     $tb_atendente_idatendente = 1;
+     $tb_cliente_idcliente = 1;
 
-    salvarPedido($conexao, $observacao, $horario_inicio, $horario_final, $status, $valor_p, $taxa_entraga, $data, $nota_atedente);
+    salvarPedido($conexao, $observacao, $horario_inicio, $horario_final, $status, $valor_p, $taxa_entrega, $data, $nota_atendente, $tb_endereco_idendereco, $tb_atendente_idatendente, $tb_cliente_idcliente) // feito
 ?>
