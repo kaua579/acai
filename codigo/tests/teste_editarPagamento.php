5dde50ded917c3f6eaaeb7f02ae1308a4fa13fd6
caroline2025 <carolzinha8641@gmail.com>
2025-06-26 10:29:25 -0300

    teste do pagamento
    só testando

--- codigo/tests/teste_editarPagamento.php
+++ codigo/tests/teste_editarPagamento.php
@@ -6,8 +6,8 @@
     $forma_p = "pix";
     $data_pagamento = "2050-12-18";
     $valor_total = 100; 
-    $idpagamento = 1;
-    $tb_pedido_idpedido = 2;
+    $idpagamento = 3;
+    $tb_pedido_idpedido = 3;
 
     editarPagamento($conexao, $forma_p, $data_pagamento, $valor_total, $tb_pedido_idpedido, $idpagamento)
 ?>
