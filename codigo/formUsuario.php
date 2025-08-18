43ba10acac862f5f6c2a894fd401f15b19a266a9
kaua579 <kauahenriquemoraissantos@gmail.com>
2025-08-18 14:27:48 -0300

    fiz o começo do css
    fiz css

--- codigo/formUsuario.php
+++ codigo/formUsuario.php
@@ -4,17 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/estilo.css">
 </head>
-<body>
-    <form action="salvarUsuario.php" method="post">
-        Nome: <br>
-        <input type="text" name="nome"> <br><br>
-        E-mail: <br>
-        <input type="text" name="email"> <br><br>
-        Senha: <br>
-        <input type="text" name="senha"> <br><br>
 
-        <input type="submit" value="Cadastrar">
+<body>
+    
+       <div class="login-box">
+        <img id="logo" src="logo.png" >
+        <h1>Login</h1>
+        <form action="salvarUsuario.php" method="post">
+
+            <input type="text" placeholder="nome"><br>
+
+            <input type="password" placeholder="Senha"><br>
+
+            <button type="submit">Login</button>
+        </form>
+        <a href="">Esqueceu a Senha?</a><br>
+        <a href="">Cadastrar-se</a>
+    </div>
     </form>
 </body>
 </html>
