249123b96552fa863664ce7704a1bd41023df113
laravtlp <laralopespugas7@gmail.com>
2025-08-25 14:45:33 -0300

    pag quem somos
    nao esta prontaaa

---
+++ codigo/historia.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilo.css">
+    <title>Document</title>
+</head>
+</head>
+<body>
+
+<div class="container">
+    <div class="image-section">
+        <img id= "mao"src="fundo.png" alt="Açai com frutas"> 
+    </div>
+
+    <div class="text-section">
+        <h1>quem nós somos ?</h1>
+        <?php
+        echo "<p>A açaíteria Alapollos localizada no Ibirapuera, surgiu quando o Walter Salles, fez uma grande viagem pelo mundo e descobriu que em todos os lugares por onde ele andou tinha uma açaíteria, mas no seu bairro faltava uma. Inspirado por essa situação e decidido a construir uma açaíteria, Salles resolveu iniciar um projeto com sua família para abrir uma açaíteria.</p>";
+
+        echo "<p>Iniciado em 2018 e finalizado em 2020, Alapollos conta com uma estrutura maravilhosa e com sabores de dar água na boca. Decidido a sempre trazer conforto aos nossos clientes, agora você pode fazer seu pedido na sua própria casa!</p>";
+        ?>
+    </div>
+</div>
+    
+</body>
+</html>
