eb130af532edc33f7171d37b4884cbc61c930b54
kaua579 <kauahenriquemoraissantos@gmail.com>
2025-08-28 16:36:04 -0300

    nem sei

--- codigo/atendentes.php
+++ codigo/atendentes.php
@@ -2,16 +2,25 @@
     require_once "conexao.php";
     require_once "funcoes.php";
 
-    $id = $_GET['id'];
+    $id = $_GET['idatendente'];
         
     $atendente = listarAtendente($conexao);
     foreach($atendente as $tb_atendente);
-        $idatendente = $atendente['idcliente']; 
+        $idatendente = $atendente['atendente']; 
 
-
-
+        // echo "<pre>";
+        //     print_r(($atendente));
+        // echo "</pre>";
+      
+        echo "<tr>";
+        echo "<td>$idatendente</td>";
+        echo "<td>$horario_chegada</td>";
+        echo "<td>$horario_saida</td>";
+        echo "<td>$descricao</td>";
+        echo "<td>$endereco</td>";
+        echo "</tr>";
 ?>
-
+   
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,6 +28,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>atendentes</title>
 </head>
+
+   
 <h1>atendente</h1>
 <body>
     
