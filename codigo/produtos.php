dba6e7e7a996bae917ea53ee2683a0895fa18200
lara <laralopespugas7@gmail.com>
2025-08-28 16:12:39 -0300

    coisas
    css produtos

---
+++ codigo/produtos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Produtos </h1>
+
+    <h3> 200 ML </h3> 
+    <h3> 300 ML </h3>
+    <h3> 500 ML </h3>     
+    <h3> 700 ML </h3>
+    <h3> 1L </h3>               
+</body>
+</html>
