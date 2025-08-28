06df0a957f728a706bdc5179c8de84b36b82d46b
caroline2025 <carolzinha8641@gmail.com>
2025-08-28 16:31:50 -0300

    cardapio
    tentando fazer o cardapio

--- codigo/cardapio.php
+++ codigo/cardapio.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cardápio</title>
 </head>
+
 <body>
-    
-</body>
+    <h1>Cardápio</h1>
+    <h2>Tamanhos</h2>
+    200mil 300mil 500mil 700ml 1L <br>
+    R$9,90 R$14,90 R$17,90 R$25,90 R$32,90 <br>
+
+    <body>
+        <h2>Acompanhamentos</h2>
+        <table>
+            <tr>
+                <th>Banana</th>
+                <th>Morango</th>
+                <th>Kiwi</th>
+            </tr>
+            <tr>
+                <td>Morango</td>
+                <td>22</td>
+                <td>São Paulo</td>
+            </tr>
+            <tr>
+                <td>Kiwi</td>
+                <td>27</td>
+                <td>Belo Horizonte</td>
+            </tr>
+        </table>
+    </body>
+
 </html>
+
+</body>
+
+</html> 
