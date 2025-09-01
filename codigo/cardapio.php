090f6d12ccd5784efa7b5567e9056a17e2cedae8
caroline2025 <carolzinha8641@gmail.com>
2025-09-01 14:43:02 -0300

    cardapio
    tentando fazer dnv

--- codigo/cardapio.php
+++ codigo/cardapio.php
@@ -4,38 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilo.css">
     <title>cardápio</title>
 </head>
 
 <body>
-    <h1>Cardápio</h1>
-    <h2>Tamanhos</h2>
-    200mil 300mil 500mil 700ml 1L <br>
-    R$9,90 R$14,90 R$17,90 R$25,90 R$32,90 <br>
+    <img id="tituloc" src="tituloc.png">
+    <h1>Tamanhos</h1>
+    <img id="tamanhos" src="tamanho.png">
+    
+    
 
-    <body>
-        <h2>Acompanhamentos</h2>
-        <table>
-            <tr>
-                <th>Banana</th>
-                <th>Morango</th>
-                <th>Kiwi</th>
-            </tr>
-            <tr>
-                <td>Morango</td>
-                <td>22</td>
-                <td>São Paulo</td>
-            </tr>
-            <tr>
-                <td>Kiwi</td>
-                <td>27</td>
-                <td>Belo Horizonte</td>
-            </tr>
-        </table>
-    </body>
 
-</html>
+     
 
-</body>
+
+
+
+
+
+
+
+
+
+     </body>
 
 </html> 
