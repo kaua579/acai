bc9d84b25dab416705b4a964364f38bf32bde78f
caroline2025 <carolzinha8641@gmail.com>
2025-09-08 14:48:32 -0300

    aruumei o cardapio
    ai

--- codigo/cardapio.php
+++ codigo/cardapio.php
@@ -5,14 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/estilo.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+
     <title>cardápio</title>
 </head>
 
 <body>
+
     <img id="tituloc" src="tituloc.png">
-    <h1>Tamanhos</h1>
-    <img id="tamanhos" src="tamanho.png">
-    
+
+    <h1 id="tamanhos">Tamanhos</h1>
+
+    <img id="tamanho" src="tamanho.png">
+
     
 
 
