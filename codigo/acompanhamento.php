9d9d36941508383f88e90b2723ed079583acb4f8
caroline2025 <carolzinha8641@gmail.com>
2025-09-11 10:54:07 -0300

    acompanhamentos.php
    fiz outra coluna com div

--- codigo/acompanhamento.php
+++ codigo/acompanhamento.php
@@ -6,8 +6,8 @@
     <title>Acompanhamentos</title>
 </head>
 <body>
-
-    <div style="display: flex; flex-direction: column; gap: 8px;">
+<div style="display: flex; gap: 40px;">
+  <div style="display: flex; flex-direction: column; gap: 8px;">
   <label>Banana <input type="checkbox" name="item1"></label>
   <label>Morango <input type="checkbox" name="item2"></label>
   <label>Kiwi <input type="checkbox" name="item3"></label>
@@ -18,7 +18,17 @@
   <label>Cereja <input type="checkbox" name="item8"></label>
 
 </div>
+<div style="display: flex; flex-direction: column; gap: 8px;">    
+    <label>Coco Ralado <input type="checkbox" name="item12"></label>
+    <label>Paçoca<input type="checkbox" name="item13"></label>
+    <label>Nutella <input type="checkbox" name="item14"></label>
+    <label>Chocolate Branco<input type="checkbox" name="item15"></label>
+    <label>Doce de Leite <input type="checkbox" name="item16"></label>
+    <label>Leite Ninho<input type="checkbox" name="item17"></label>
+    <label>Whey<input type="checkbox" name="item18"></label>
 
+  </div>
+</div>
 
 
 </body>
