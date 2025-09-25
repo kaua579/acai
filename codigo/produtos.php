bd452bab332a50b1d04cff643e1ba55e2570e218
laravtlp <laralopespugas7@gmail.com>
2025-09-25 09:58:12 -0300

    prod
    .

--- codigo/produtos.php
+++ codigo/produtos.php
@@ -6,45 +6,43 @@
     <link rel="stylesheet" href="./css/estilo.css">
     <title>Document</title>
 </head>
-<body>
-    <img id="titulop" src="titulop.png" alt="Título Principal">
-    <div class="container">
+<body>  
+<div class="fundo">
+    <img id="titulop" src="titulop.png" alt="Título Principal" class="titulo">
 
         <form action="comprar.php" method="post" class="produtos">
 
             <div class="produto">
                 <input type="checkbox" name="produto[]" value="200ml">
-                <img id= "Açai 200ml" src="acai200ml.png" >
+                <img id= "Açai200ml" src="acai200ml.png" >
                 <p>200 ML</p>
             </div>
 
             <div class="produto">
                 <input type="checkbox" name="produto[]" value="300ml">
-                <img id="acai 300ml" src="acai300ml.png">
+                <img id="acai300ml" src="acai300ml.png">
                 <p>300 ML</p>
             </div>
 
             <div class="produto">
                 <input type="checkbox" name="produto[]" value="500ml">
-                <img id="acai 500ml" src="acai500ml.png">
+                <img id="acai500ml" src="acai500ml.png">
                 <p>500 ML</p>
             </div>
 
             <div class="produto">
                 <input type="checkbox" name="produto[]" value="700ml">
-                <img id="acai 700ml" src="acai700ml.png">
+                <img id="acai700ml" src="acai700ml.png">
                 <p>700 ML</p>
             </div>
 
             <div class="produto">
                 <input type="checkbox" name="produto[]" value="1l">
-                <img id="acai 1l" src="acai1l.png">
+                <img id="acai1l" src="acai1l.png">
                 <p>1 L</p>
             </div>
 
               <a href="acompanhamento.php">Comprar</a> <br><br>
-
-                
 
         </form>
     </div>            
