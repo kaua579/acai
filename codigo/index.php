b3e00acf4ee55f476afa6eb03ae2de3e9444a9f2
caroline2025 <carolzinha8641@gmail.com>
2025-09-25 16:49:36 -0300

    tentei fazer a pagina de login
    não deu certo

--- codigo/index.php
+++ codigo/index.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilo.css">
+
     <title>Document</title>
 </head>
 <body>
     <h1>Acesso ao sistema</h1>
+    
 
     <form action="verificarLogin.php" method="post">
         E-mail: <br>
@@ -14,7 +17,7 @@
         Senha: <br>
         <input type="text" name="senha"> <br><br>
 
-        <a href="formUsuario.php">Primeiro acesso</a> <br><br>
+        <!-- <a href="formUsuario.php">Primeiro acesso</a> <br><br> -->
 
         <input type="submit" value="Acessar">
     </form>
