295c8a49a56090d6856c2b882d0acb7119705381
caroline2025 <carolzinha8641@gmail.com>
2025-10-06 19:11:14 -0300

    kaua
    fez merda no cardapio

--- codigo/cardapio.php
+++ codigo/cardapio.php
@@ -2,40 +2,40 @@
 <html lang="pt-br">
 
 <head>
-    <!-- pedir o lucas -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cardápio - Açaí</title>
+    <title>Cardápio de Açaí</title>
     <link rel="stylesheet" href="./css/estilo.css">
+      
 </head>
 
-<body class="fundoca">
-    <div class="wrapper">
+<body class="corpo-cardapio">
+    <div class="corpo">
         <div class="menu">
-            <header class="menu-header">
-                <h1>Cardápio</h1>
+            <header>
+                <h1> Cardápio </h1>
             </header>
 
-            <section class="sizes">
+            <section class="tamanho">
                 <h2>Tamanhos</h2>
-                <div class="size-items">
-                    <div class="size-item">
+                <div class="tamanho2">
+                    <div class="tamanho-item">
                         <h3>200ml</h3>
                         <p>R$9,90</p>
                     </div>
-                    <div class="size-item">
+                    <div class="tamanho-item">
                         <h3>300ml</h3>
                         <p>R$14,90</p>
                     </div>
-                    <div class="size-item">
+                    <div class="tamanho-item">
                         <h3>500ml</h3>
                         <p>R$17,90</p>
                     </div>
-                    <div class="size-item">
+                    <div class="tamanho-item">
                         <h3>700ml</h3>
                         <p>R$25,90</p>
                     </div>
-                    <div class="size-item">
+                    <div class="tamanho-item">
                         <h3>1L</h3>
                         <p>R$32,90</p>
                     </div>
@@ -45,35 +45,35 @@
             <section class="acompanhamentos">
                 <h2>Acompanhamentos</h2>
                 <div class="acomp-itens">
-                    <div class="acomp-item">Banana - R$2,50</div>
-                    <div class="acomp-item">Morango - R$2,50</div>
-                    <div class="acomp-item">Kiwi - R$2,50</div>
-                    <div class="acomp-item">Granola - R$2,50</div>
-                    <div class="acomp-item">Castanha-de-caju - R$2,50</div>
-                    <div class="acomp-item">Amendoim - R$2,50</div>
-                    <div class="acomp-item">Mel - R$2,50</div>
-                    <div class="acomp-item">Coco Ralado - R$2,50</div>
-                    <div class="acomp-item">Paçoca - R$2,50</div>
-                    <div class="acomp-item">Nutella - R$3,00</div>
-                    <div class="acomp-item">Chocolate Branco - R$3,00</div>
-                    <div class="acomp-item">Doce de Leite - R$3,00</div>
-                    <div class="acomp-item">Ovomaltine - R$3,00</div>
-                    <div class="acomp-item">Leite Ninho - R$3,00</div>
-                    <div class="acomp-item">Cereja - R$3,00</div>
-                    <div class="acomp-item">Whey - R$3,00</div>
+                    <div class="acomp-item"> Banana - R$2,50</div>
+                    <div class="acomp-item"> Morango - R$2,50</div>
+                    <div class="acomp-item"> Kiwi - R$2,50</div>
+                    <div class="acomp-item"> Granola - R$2,50</div>
+                    <div class="acomp-item"> Castanha-de-caju - R$2,50</div>
+                    <div class="acomp-item"> Amendoim - R$2,50</div>
+                    <div class="acomp-item"> Mel - R$2,50</div>
+                    <div class="acomp-item"> Coco Ralado - R$2,50</div>
+                    <div class="acomp-item"> Paçoca - R$2,50</div>
+                    <div class="acomp-item"> Nutella - R$3,00</div>
+                    <div class="acomp-item"> Chocolate Branco - R$3,00</div>
+                    <div class="acomp-item"> Doce de Leite - R$3,00</div>
+                    <div class="acomp-item"> Ovomaltine - R$3,00</div>
+                    <div class="acomp-item"> Leite Ninho - R$3,00</div>
+                    <div class="acomp-item"> Cereja - R$3,00</div>
+                    <div class="acomp-item"> Whey - R$3,00</div>
                 </div>
             </section>
 
             <section class="cobertura">
                 <h2>Coberturas</h2>
                 <div class="cobert-itens">
-                    <div class="cobert-item">Leite Condensado - R$1,50</div>
-                    <div class="cobert-item">Doce de Leite - R$1,50</div>
-                    <div class="cobert-item">Calda de Morango - R$1,50</div>
-                    <div class="cobert-item">Calda de Caramelo - R$1,50</div>
-                    <div class="cobert-item">Calda de Chocolate - R$1,50</div>
-                    <div class="cobert-item">Meio Amargo - R$1,50</div>
-                    <div class="cobert-item">Chantilly - R$1,50</div>
+                    <div class="cobert-item"> Leite Condensado - R$1,50</div>
+                    <div class="cobert-item"> Doce de Leite - R$1,50</div>
+                    <div class="cobert-item"> Calda de Morango - R$1,50</div>
+                    <div class="cobert-item"> Calda de Caramelo - R$1,50</div>
+                    <div class="cobert-item"> Calda de Chocolate - R$1,50</div>
+                    <div class="cobert-item"> Meio Amargo - R$1,50</div>
+                    <div class="cobert-item"> Chantilly - R$1,50</div>
                 </div>
             </section>
         </div>
