b2938f3c3d5ff736eb2c98b127a2496ffa7e7387
laravtlp <laralopespugas7@gmail.com>
2025-10-06 14:44:59 -0300

    quem somos pag
    ainda nao esta pronto

--- codigo/historia.php
+++ codigo/historia.php
@@ -8,30 +8,17 @@
 </head>
 </head>
 <body>
- <div class="historia">
-        <!-- Lado da imagem -->
-        <div class="lado-esquerdo">
-            <img src="fundohistoria.png" alt="Açaí">
-        </div>
-
-        <!-- Lado do texto -->
-        <div class="lado-direito">
-           <img id="titulohis" src="titulohis.png" alt="Título Principal">
+ <div class="img">
+    <div class="historia">
+        <div>
+            <h1>Quem nós somos?</h1>
             <p>
-                A AÇAÍTERIA ALAPOLLOS LOCALIZADA NO IBIRAPUERA, SURGIU QUANDO O WALTER
-                SALLES, FEZ UMA GRANDE VIAGEM PELO MUNDO E DESCOBRIU QUE EM TODOS OS LUGARES 
-                POR ONDE ELE ANDOU TINHA UMA AÇAÍTERIA, MAS NO SEU BAIRRO FALTAVA UMA. 
-                INSPIRADO POR ESSA SITUAÇÃO E DECIDIDO A CONSTRUIR UMA AÇAÍTERIA , 
-                SALLES RESOLVEU INICIAR UM PROJETO COM SUA FAMÍLIA PARA ABRIR UMA AÇAÍTERIA.
-            </p>
-            <p>
-                INICIADO EM 2018 E FINALIZADO EM 2020, ALAPOLLOS CONTA COM UMA 
-                ESTRUTURA MARAVILHOSA E COM SABORES DE DAR ÁGUA NA BOCA. DECIDIDO A 
-                SEMPRE TRAZER CONFORTO AOS NOSSOS CLIENTES, AGORA VOCÊ PODE FAZER 
-                SEU PEDIDO NA SUA PRÓPRIA CASA!
+                A Açaíteria Alapollos localizada no Ibirapuera surgiu quando o Walter Salles fez uma grande viagem pelo mundo e descobriu que em todos os lugares por onde ele andou tinha uma açaíteria, mas no seu bairro faltava uma. Inspirado por essa situação e decidido a construir uma açaíteria, Salles resolveu iniciar um projeto com sua família para abrir uma açaíteria.<br><br>
+                Iniciado em 2018 e finalizado em 2020, Alapollos conta com uma estrutura maravilhosa e com sabores de dar água na boca. Decidido a sempre trazer conforto aos nossos clientes, agora você pode fazer seu pedido na sua própria casa!
             </p>
         </div>
     </div>
+</div>
     
 </body>
 </html>
--- codigo/produtos.php
+++ codigo/produtos.php
@@ -8,43 +8,38 @@
 </head>
 <body>  
 <div class="fundo">
-    <img id="titulop" src="titulop.png" alt="Título Principal" class="titulo">
+    <img src="titulop.png" class="titulo-img"> 
 
-        <form action="comprar.php" method="post" class="produtos">
+    <div class="produtos">
+      <div class="produto">
+        <img src="acai200ml.png" alt="200 ml">
+        <p>200 ML</p>
+      </div>
 
-            <div class="produto">
-                <input type="checkbox" name="produto[]" value="200ml">
-                <img id= "Açai200ml" src="acai200ml.png" >
-                <p>200 ML</p>
-            </div>
+      <div class="produto">
+        <img src="acai300ml.png" alt="300 ml">
+        <p>300 ML</p>
+      </div>
 
-            <div class="produto">
-                <input type="checkbox" name="produto[]" value="300ml">
-                <img id="acai300ml" src="acai300ml.png">
-                <p>300 ML</p>
-            </div>
+      <div class="produto">
+        <img src="acai500ml.png" alt="500 ml">
+        <p>500 ML</p>
+      </div>
 
-            <div class="produto">
-                <input type="checkbox" name="produto[]" value="500ml">
-                <img id="acai500ml" src="acai500ml.png">
-                <p>500 ML</p>
-            </div>
+      <div class="produto">
+        <img src="acai700ml.png" alt="700 ml">
+        <p>700 ML</p>
+      </div>
 
-            <div class="produto">
-                <input type="checkbox" name="produto[]" value="700ml">
-                <img id="acai700ml" src="acai700ml.png">
-                <p>700 ML</p>
-            </div>
+      <div class="produto">
+        <img src="acai1l.png" alt="1 L">
+        <p>1 L</p>
+      </div>
+    </div>
 
-            <div class="produto">
-                <input type="checkbox" name="produto[]" value="1l">
-                <img id="acai1l" src="acai1l.png">
-                <p>1 L</p>
-            </div>
-
-              <a href="acompanhamento.php">Comprar</a> <br><br>
-
-        </form>
-    </div>            
+    <div class="acao">
+      <a href="acompanhamento.php" class="btn-comprar">Comprar</a>
+    </div>
+  </div>
 </body>
 </html>
