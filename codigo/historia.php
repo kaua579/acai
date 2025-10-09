7df1cfdbdf7d4e19c6311f925c6308957ce53832
laravtlp <laralopespugas7@gmail.com>
2025-10-09 10:46:58 -0300

    pagina de produto e a historia
    ainda não estão finalizadas

--- codigo/historia.php
+++ codigo/historia.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/estilo.css">
     <title>Document</title>
 </head>
-</head>
 <body>
- <div class="img">
-    <div class="historia">
-        <div>
-            <h1>Quem nós somos?</h1>
-            <p>
-                A Açaíteria Alapollos localizada no Ibirapuera surgiu quando o Walter Salles fez uma grande viagem pelo mundo e descobriu que em todos os lugares por onde ele andou tinha uma açaíteria, mas no seu bairro faltava uma. Inspirado por essa situação e decidido a construir uma açaíteria, Salles resolveu iniciar um projeto com sua família para abrir uma açaíteria.<br><br>
-                Iniciado em 2018 e finalizado em 2020, Alapollos conta com uma estrutura maravilhosa e com sabores de dar água na boca. Decidido a sempre trazer conforto aos nossos clientes, agora você pode fazer seu pedido na sua própria casa!
-            </p>
+<div class="pagina">
+    <div class="conteudo">
+        <img src="fundohistoria.png" class="fundoh"> 
+            <div class="texto">
+                <img src="titulohis.png" class="titulo"> 
+                <p class="historia">
+                    A Açaíteria Alapollos localizada no Ibirapuera, surgiu quando o Walter Salles, 
+                    fez uma grande viagem pelo mundo e descobriu que em todos os lugares por onde 
+                    ele andou tinha uma açaiteria, mas no seu bairro faltava uma. Inspirado por essa 
+                    situação e decidido a construir uma açaiteria , Salles resolveu iniciar um projeto 
+                    com sua família para abrir uma açaiteria. 
+                    Iniciado em 2018 e finalizado em 2020, Alapollos conta com uma estrutura maravilhosa
+                    e com sabores de dar água na boca. Decidido a sempre trazer conforto aos nossos clientes, 
+                    agora você pode fazer seu pedido na sua própria casa!
+                </p>
+            </div>
         </div>
-    </div>
 </div>
-    
+
 </body>
 </html>
--- codigo/produtos.php
+++ codigo/produtos.php
@@ -1,45 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/estilo.css">
-    <title>Document</title>
+    <title>Produtos - Açaí</title>
 </head>
-<body>  
-<div class="fundo">
-    <img src="titulop.png" class="titulo-img"> 
+<body>
+<div class="pag">  
+    <div class="fundo">
+        
+        <img src="titulop.png" class="titulo-img"> 
 
-    <div class="produtos">
-      <div class="produto">
-        <img src="acai200ml.png" alt="200 ml">
-        <p>200 ML</p>
-      </div>
+        <div class="produtos">
+           
+            <div class="produto">
+                <img src="acai200ml.png" alt="200 ml">
+                <p>200 ML</p>
+                <a href="acompanhamento.php?produto=200ml" class="btn-comprar">Comprar</a>
+            </div>
 
-      <div class="produto">
-        <img src="acai300ml.png" alt="300 ml">
-        <p>300 ML</p>
-      </div>
+            <div class="produto">
+                <img src="acai300ml.png" alt="300 ml">
+                <p>300 ML</p>
+                <a href="acompanhamento.php?produto=300ml" class="btn-comprar">Comprar</a>
+            </div>
 
-      <div class="produto">
-        <img src="acai500ml.png" alt="500 ml">
-        <p>500 ML</p>
-      </div>
+            <div class="produto">
+                <img src="acai500ml.png" alt="500 ml">
+                <p>500 ML</p>
+                <a href="acompanhamento.php?produto=500ml" class="btn-comprar">Comprar</a>
+            </div>
 
-      <div class="produto">
-        <img src="acai700ml.png" alt="700 ml">
-        <p>700 ML</p>
-      </div>
+            <div class="produto">
+                <img src="acai700ml.png" alt="700 ml">
+                <p>700 ML</p>
+                <a href="acompanhamento.php?produto=700ml" class="btn-comprar">Comprar</a>
+            </div>
 
-      <div class="produto">
-        <img src="acai1l.png" alt="1 L">
-        <p>1 L</p>
-      </div>
+            <div class="produto">
+                <img src="acai1l.png" alt="1 L">
+                <p>1 L</p>
+                <a href="acompanhamento.php?produto=1l" class="btn-comprar">Comprar</a>
+            </div>
+        </div>
     </div>
-
-    <div class="acao">
-      <a href="acompanhamento.php" class="btn-comprar">Comprar</a>
-    </div>
-  </div>
+</div>
 </body>
 </html>
