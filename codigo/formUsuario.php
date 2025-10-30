9eda0cc7b86c089160d27e410c7543a077624bd3
caroline2025 <carolzinha8641@gmail.com>
2025-10-30 08:04:02 -0300

    form.Usuario
    lucas arrumou o java script dessa pagina falta só a senha

--- codigo/formUsuario.php
+++ codigo/formUsuario.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="../jquery-3.7.1.min.js"></script>
-    <script src="../jquery.validate.min.js"></script>
+    <script src="./jquery-3.7.1.min.js"></script>
+    <script src="./jquery.validate.min.js"></script>
 </head>
     <script>
         $(document).ready(function () {
@@ -39,7 +39,7 @@
         }
     </style>
 <body>
-    <form action="salvarUsuario.php" method="post">
+    <form id="formUsuario" action="salvarUsuario.php" method="post">
         Nome: <br>
         <input type="text" name="nome" id="nome"> <br><br>
         E-mail: <br>
